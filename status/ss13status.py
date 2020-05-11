2b5c9f055c01f4bb26e3dadf04efbbc1126ae1db
Mark Suckerberg <tommjd54321@outlook.com>
2020-05-11 18:24:41 -0500

    he says he'd like to meet us

--- status/ss13status.py
+++ status/ss13status.py
@@ -479,7 +479,7 @@ class SS13Status(commands.Cog):
         Queries the server for information
         """
 
-        server = self.config.server()
+        server = socket.gethostbyname(await self.config.server())
         port = self.config.game_port()
 
         message = {"query": querystr}
