e6893b10d07f6181a49f8a99e72adf74dc593c2d
Mark Suckerberg <tommjd54321@outlook.com>
2020-05-13 15:22:32 -0500

    I give up

--- ss13commands/ss13commands.py
+++ ss13commands/ss13commands.py
@@ -209,15 +209,8 @@ class SS13Commands(commands.Cog):
         """
         info = await self.topic_query_server(ctx, querystr=f"namecheck={target}")
         if(info):
-            name = str(info.split("(")[0])
-            realname = str((info.split(") ")[0]).split("(")[1])
-            ckeyinfo = str(info.split(") ")[1]) #this is hacky as hell and I hate myself for it
-            embed = discord.Embed(name=f"Results for {target}:")
-            embed.add_field("Name:", name)
-            embed.add_field("Real name:", realname)
-            embed.add_field("Ckey:", ckeyinfo)
             #TODO: Split recieved data into different embed fields
-            await ctx.send(embed=embed)
+            await ctx.send(embed=discord.Embed(name=f"Results for {target}:", description=f"{info}"))
 
     @commands.guild_only()
     @commands.command()
