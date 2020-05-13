d84104601f2b8a23e7f033bf860ca4e8dbd22bfa
Mark Suckerberg <tommjd54321@outlook.com>
2020-05-13 15:18:10 -0500

    hatred

--- ss13commands/ss13commands.py
+++ ss13commands/ss13commands.py
@@ -209,9 +209,9 @@ class SS13Commands(commands.Cog):
         """
         info = await self.topic_query_server(ctx, querystr=f"namecheck={target}")
         if(info):
-            name = info.split("(")[0]
-            realname = (info.split(") ")[0]).split("(")[1]
-            ckeyinfo = info.split(") ")[1] #this is hacky as hell and I hate myself for it
+            name = str(info.split("(")[0])
+            realname = str((info.split(") ")[0]).split("(")[1])
+            ckeyinfo = str(info.split(") ")[1]) #this is hacky as hell and I hate myself for it
             embed = discord.Embed(name=f"Results for {target}:")
             embed.add_field("Name:", name)
             embed.add_field("Real name:", realname)
