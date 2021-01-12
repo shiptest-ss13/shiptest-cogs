680fae9a576313110b0ad904e2688a5389859136
Mark Suckerberg <tommjd54321@outlook.com>
2021-01-12 12:52:36 -0600

    oh fuck

--- status/ss13status.py
+++ status/ss13status.py
@@ -404,7 +404,7 @@ class SS13Status(commands.Cog):
             else:
                 embed.add_field(name="Shuttle Status", value="Refueling", inline=True)
             if "mining_map_name" in data:
-                embed.add_field(name="Shuttle Status", value=str.title(*data['mining_map_name']), inline=True)
+                embed.add_field(name="Mining Map", value=str.title(*data['mining_map_name']), inline=True)
             embed.add_field(name="Players", value=players, inline=True)
             embed.add_field(name="Admins", value=int(*data['admins']), inline=True)
             embed.add_field(name="Round Duration", value=duration, inline=True)
