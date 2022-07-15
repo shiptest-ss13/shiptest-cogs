05284e95adb3b5751ed5cba48eddf945a8b1db17
Zephyr <12817816+ZephyrTFA@users.noreply.github.com>
2022-07-15 13:25:47 -0400

    Update ss13mon.py

--- ss13mon/ss13mon.py
+++ ss13mon/ss13mon.py
@@ -117,8 +117,8 @@ class SS13Mon(commands.Cog):
 		roundid = int(*status["round_id"])
 		servtitle = str(*status["version"])
 		await self.config.guild(guild).last_roundid.set(roundid)
-		duration = int(*status['round_duration'])
-		duration = str(datetime.timedelta(seconds=duration))
+		duration = "{}s".format(str(int(*status['round_duration'])))
+		# duration = str(datetime.timedelta(seconds=duration))
 		player_count = int(*status["players"])
 		time_dilation_avg = float(*status["time_dilation_avg"])
 		players: list[str] = (await self.query_server("localhost", 41372, "?whoIs"))["players"]
