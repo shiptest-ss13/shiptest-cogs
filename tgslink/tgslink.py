235b3b715ddf54706d391db434c4e6f03d72643a
ZephyrTFA <matthew@tfaluc.com>
2022-08-26 19:41:27 -0400

    wip

--- tgslink/tgslink.py
+++ tgslink/tgslink.py
@@ -38,11 +38,24 @@ class TGSLink(commands.Cog):
 	async def tgslink(self, ctx): pass
 
 	@tgslink.command()
-	async def login(self, ctx, username, password):
+	async def login(self, ctx, username = None, password = None):
 		cfg = self._config.member(ctx.author)
-		if(await cfg.pass_remember()):
+		if(username ^ password):
+			await ctx.reply("Either both username and password must be supplied or neither!")
+			await self.try_delete(ctx.message)
+			return
+
+		if(await cfg.pass_remember() and username):
 			await cfg.pass_username.set(username)
 			await cfg.pass_password.set(password)
+
+		if(not username):
+			username = await cfg.pass_username()
+			password = await cfg.pass_password()
+			if(not username or not password):
+				await ctx.reply("Login information is not saved!")
+				await self.try_delete(ctx.message)
+				return
 
 		try:
 			resp = tgs_login(self.address(ctx.guild), username, password)
@@ -52,3 +65,15 @@ class TGSLink(commands.Cog):
 		except:
 			await ctx.reply("Failed to log in.")
 		await self.try_delete(ctx.message)
+
+	@tgslink.group()
+	async def config(self, ctx): pass
+
+	@config.command()
+	async def remember_login(self, ctx):
+		cfg = self._config.member(ctx.author)
+		target = not await cfg.pass_remember()
+		if(not target):
+			await cfg.pass_username.set(None)
+			await cfg.pass_password.set(None)
+		await ctx.reply("Login details are {} being saved.".format(["no longer", "now"][target]))
