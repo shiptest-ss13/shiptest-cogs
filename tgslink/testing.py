0c8c41a64be4ce1073156ee98c0b29f0f5393df2
Zephyr <12817816+ZephyrTFA@users.noreply.github.com>
2022-08-27 17:38:48 -0400

    Delete testing.py

--- tgslink/testing.py
+++
@@ -1,8 +0,0 @@
-from py_tgs.tgs_api_models import *
-from py_tgs.tgs_api_defs import *
-
-address = "https://tgs.shiptest.net/"
-resp = tgs_login(address, "dragon", "3VSeMT2L331kJ^!9Ay")
-print(resp.Bearer)
-
-# ?tgslink login dragon BP2FfIKQrr7h
