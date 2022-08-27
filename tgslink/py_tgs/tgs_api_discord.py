3c6493ad6d9965db373564bfcc40c60badd0e7a7
ZephyrTFA <matthew@tfaluc.com>
2022-08-26 20:51:51 -0400

    better

--- tgslink/py_tgs/tgs_api_discord.py
+++ tgslink/py_tgs/tgs_api_discord.py
@@ -9,9 +9,9 @@ def job_to_embed(job: TgsModel_JobResponse) -> Embed:
 	if(state is JobState.Running): color = Colour.blue()
 
 	emb = Embed(type="rich", title="Job #{} Information".format(job.Id), timestamp=datetime.utcnow(), color=color)
-	emb.add_field(name="Job Information", value="Started:{}\nBy: {}\nDesc: {}".format(job.StartedAt, job.StartedBy.Name, job.Description), inline=True)
+	emb.add_field(name="Job Information", value="Started:{}\nBy: {}\nDesc: {}".format(job.StartedAt, job.StartedBy.Name, job.Description), inline=False)
 	if(state is not JobState.Running):
-		emb.add_field(name="Job Completion Information", value="Stopped: {}\nCanceled: {}\nBy: {}".format(job.StoppedAt, job.Cancelled, "N/A" if job.CancelledBy is None else job.CancelledBy.Name), inline=True)
+		emb.add_field(name="Job Completion Information", value="Stopped: {}\nCanceled: {}\nBy: {}".format(job.StoppedAt, job.Cancelled, "N/A" if job.CancelledBy is None else job.CancelledBy.Name), inline=False)
 	if(state is JobState.Errored):
-		emb.add_field(name="Error Information", value="Error #{}\n{}".format(job.ErrorCode, job.ExceptionDetails), inline=True)
+		emb.add_field(name="Error Information", value="Error #{}\n{}".format(job.ErrorCode, job.ExceptionDetails), inline=False)
 	return emb
