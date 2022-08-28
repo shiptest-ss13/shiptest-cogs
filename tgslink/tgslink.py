b846ccba8ed900507ec897d6b6e3ee23b8714893
ZephyrTFA <matthew@tfaluc.com>
2022-08-28 06:21:43 -0400

    update tgslink

--- tgslink/tgslink.py
+++ tgslink/tgslink.py
@@ -1,207 +1,205 @@
 import asyncio
 from datetime import datetime
-from time import sleep
 from redbot.core import commands, Config, checks
 import logging
-from discord import Message, Emoji
+from discord import Message
 
 from tgslink.py_tgs.tgs_api_discord import job_to_embed
-from tgslink.py_tgs.tgs_api_models import TgsModel_ErrorMessageResponse, TgsModel_TokenResponse
+from tgslink.py_tgs.tgs_api_models import TgsModel_ErrorMessageResponse
 
-from .py_tgs.tgs_api_defs import tgs_dm_deploy, tgs_job_cancel, tgs_job_get, tgs_login, tgs_repo_status, tgs_repo_update, tgs_repo_update_tms
+from .py_tgs.tgs_api_defs import tgs_dm_deploy, tgs_job_cancel, tgs_job_get, tgs_login, tgs_repo_status, tgs_repo_update_tms
 
 log = logging.getLogger("red.tgslink")
 
+
 class TGSLink(commands.Cog):
-	_config: Config
-	def __init__(self, bot: commands.Bot) -> None:
-		self.bot = bot
-		self._config = Config.get_conf(self, identifier=85416841231161, force_registration=True)
+    _config: Config
 
-		def_guild = {
-			"address": None,
-		}
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+        self._config = Config.get_conf(self, identifier=85416841231161, force_registration=True)
 
-		def_member = {
-			"pass_remember": None,
-			"pass_username": None,
-			"pass_password": None,
-			"token_bearer": None,
-			"token_expiration": None, # this is a timestamp
-			"token_gh": None,
-		}
+        def_guild = {
+            "address": None,
+        }
 
-		self._config.register_guild(**def_guild)
-		self._config.register_member(**def_member)
+        def_member = {
+            "pass_remember": None,
+            "pass_username": None,
+            "pass_password": None,
+            "token_bearer": None,
+            "token_expiration": None,  # this is a timestamp
+            "token_gh": None,
+        }
 
-	async def get_address(self, guild): return await self._config.guild(guild).address()
-	async def get_token(self, ctx):
-		cfg = self._config.member(ctx.author)
-		exp = await cfg.token_expiration()
-		dif = (exp - datetime.utcnow().timestamp()) if exp is not None else None
-		if(not dif and not await self._login(ctx)):
-			log.info("token expired and we failed to refresh it")
-			return None
-		return await cfg.token_bearer()
+        self._config.register_guild(**def_guild)
+        self._config.register_member(**def_member)
 
-	async def try_delete(self, message):
-		try:
-			await message.delete()
-		except:
-			await message.reply("Failed to delete message, you must delete it manually!")
+    async def get_address(self, guild):
+        return await self._config.guild(guild).address()
 
-	@commands.group()
-	async def tgslink(self, ctx): pass
+    async def get_token(self, ctx):
+        cfg = self._config.member(ctx.author)
+        exp = await cfg.token_expiration()
+        dif = (exp - datetime.utcnow().timestamp()) if exp is not None else None
+        if not dif and not await self._login(ctx):
+            log.info("token expired and we failed to refresh it")
+            return None
+        return await cfg.token_bearer()
 
-	async def _login(self, ctx, username = None, password = None) -> bool:
-		cfg = self._config.member(ctx.author)
+    async def try_delete(self, message: Message):
+        try:
+            await message.delete()
+        except Exception:
+            await message.reply("Failed to delete message, you must delete it manually!")
 
-		if(username is None or password is None):
-			if(not await cfg.pass_remember()): return False
-			username = await cfg.pass_username()
-			password = await cfg.pass_password()
-			if(username is None or password is None): return False
+    @commands.group()
+    async def tgslink(self, ctx):
+        pass
 
-		try:
-			resp = tgs_login(await self.get_address(ctx.guild), username, password)
-			await cfg.token_bearer.set(resp.Bearer)
-			await cfg.token_expiration.set(resp.ExpiresAt.timestamp())
-			return True
-		except Exception as e:
-			log.exception(e)
-			return False
+    async def _login(self, ctx, username=None, password=None) -> bool:
+        cfg = self._config.member(ctx.author)
 
-	@tgslink.command()
-	async def login(self, ctx, username = None, password = None):
-		cfg = self._config.member(ctx.author)
-		if((username is not None) ^ (password is not None)):
-			await ctx.reply("Either both username and password must be supplied or neither!")
-			await self.try_delete(ctx.message)
-			return
+        if username is None or password is None:
+            if not await cfg.pass_remember():
+                return False
+            username = await cfg.pass_username()
+            password = await cfg.pass_password()
+            if username is None or password is None:
+                return False
 
-		if(await cfg.pass_remember() and username is not None):
-			log.info("saving login information for {}".format(ctx.author))
-			await cfg.pass_username.set(username)
-			await cfg.pass_password.set(password)
+        try:
+            resp = tgs_login(await self.get_address(ctx.guild), username, password)
+            await cfg.token_bearer.set(resp.Bearer)
+            await cfg.token_expiration.set(resp.ExpiresAt.timestamp())
+            return True
+        except Exception as e:
+            log.exception(e)
+            return False
 
-		if(username is None):
-			log.info("no username provided")
-			username = await cfg.pass_username()
-			password = await cfg.pass_password()
-			if(username is None or password is None):
-				await ctx.reply("Login information is not saved!")
-				await self.try_delete(ctx.message)
-				return
+    @tgslink.command()
+    async def login(self, ctx, username=None, password=None):
+        cfg = self._config.member(ctx.author)
+        if (username is not None) ^ (password is not None):
+            await ctx.reply("Either both username and password must be supplied or neither!")
+            await self.try_delete(ctx.message)
+            return
 
-		try:
-			if(await self._login(ctx, username, password)): await ctx.reply("Logged in")
-			else: await ctx.reply("Failed to log in.")
-		except Exception as a:
-			await ctx.reply("Failed to log in.")
-		await self.try_delete(ctx.message)
+        if await cfg.pass_remember() and username is not None:
+            log.info("saving login information for {}".format(ctx.author))
+            await cfg.pass_username.set(username)
+            await cfg.pass_password.set(password)
 
-	@tgslink.group()
-	async def config(self, ctx): pass
+        if username is None:
+            log.info("no username provided")
+            username = await cfg.pass_username()
+            password = await cfg.pass_password()
+            if username is None or password is None:
+                await ctx.reply("Login information is not saved!")
+                await self.try_delete(ctx.message)
+                return
 
-	@config.command()
-	async def remember_login(self, ctx):
-		cfg = self._config.member(ctx.author)
-		target = not await cfg.pass_remember()
-		if(not target):
-			await cfg.pass_username.set(None)
-			await cfg.pass_password.set(None)
-		await cfg.pass_remember.set(target)
-		await ctx.reply("Login details are {} being saved.".format(["no longer", "now"][target]))
+        try:
+            if await self._login(ctx, username, password):
+                await ctx.reply("Logged in")
+            else:
+                await ctx.reply("Failed to log in.")
+        except Exception:
+            await ctx.reply("Failed to log in.")
+        await self.try_delete(ctx.message)
 
-	@config.command()
-	@checks.admin()
-	async def address(self, ctx, address):
-		cfg = self._config.guild(ctx.guild)
-		await cfg.address.set(address)
-		await ctx.reply("Updated address!")
+    @tgslink.group()
+    async def config(self, ctx):
+        pass
 
-	@config.command()
-	async def gh_token(self, ctx, gh_token):
-		cfg = self._config.member(ctx.author)
-		await cfg.gh_token.set(gh_token)
-		await ctx.reply("Updated your GH Token")
-		await self.try_delete(ctx.message)
+    @config.command()
+    async def remember_login(self, ctx):
+        cfg = self._config.member(ctx.author)
+        target = not await cfg.pass_remember()
+        if not target:
+            await cfg.pass_username.set(None)
+            await cfg.pass_password.set(None)
+        await cfg.pass_remember.set(target)
+        await ctx.reply("Login details are {} being saved.".format(["no longer", "now"][target]))
 
-	@tgslink.group()
-	async def job(self, ctx): pass
+    @config.command()
+    @checks.admin()
+    async def address(self, ctx, address):
+        cfg = self._config.guild(ctx.guild)
+        await cfg.address.set(address)
+        await ctx.reply("Updated address!")
 
-	@job.command()
-	async def get(self, ctx, instance, job_id):
-		resp = tgs_job_get(await self.get_address(ctx.guild), await self.get_token(ctx), instance, job_id)
-		await ctx.reply(embed=job_to_embed(resp))
+    @config.command()
+    async def gh_token(self, ctx, gh_token):
+        cfg = self._config.member(ctx.author)
+        await cfg.gh_token.set(gh_token)
+        await ctx.reply("Updated your GH Token")
+        await self.try_delete(ctx.message)
 
-	@job.command()
-	async def cancel(self, ctx, instance, job_id):
-		try:
-			resp = tgs_job_cancel(await self.get_address(ctx.guild), await self.get_token(ctx), instance, job_id)
-			await ctx.reply(embed=job_to_embed(resp))
-		except TgsModel_ErrorMessageResponse as err:
-			await ctx.reply("Failed to cancel job: ({})='{}'".format(err._status_code, err.Message))
+    @tgslink.group()
+    async def job(self, ctx):
+        pass
 
-	@tgslink.group()
-	async def dm(self, ctx): pass
+    @job.command()
+    async def get(self, ctx, instance, job_id):
+        resp = tgs_job_get(await self.get_address(ctx.guild), await self.get_token(ctx), instance, job_id)
+        await ctx.reply(embed=job_to_embed(resp))
 
-	@dm.command()
-	async def deploy(self, ctx: commands.Context, instance = 1):
-		try:
-			job = tgs_dm_deploy(await self.get_address(ctx.guild), await self.get_token(ctx), instance)
-			msg: Message = await ctx.reply("```Caching```\n")
-			await msg.add_reaction("❌")
-			msg_id = msg.id
+    @job.command()
+    async def cancel(self, ctx, instance, job_id):
+        try:
+            resp = tgs_job_cancel(await self.get_address(ctx.guild), await self.get_token(ctx), instance, job_id)
+            await ctx.reply(embed=job_to_embed(resp))
+        except TgsModel_ErrorMessageResponse as err:
+            await ctx.reply("Failed to cancel job: ({})='{}'".format(err._status_code, err.Message))
 
-			we_canceled = False
-			while(not job.StoppedAt):
-				await asyncio.sleep(0.5)
-				if(not we_canceled):
-					msg = await ctx.fetch_message(msg_id)
-					for reaction in msg.reactions:
-						emoji_str = str(reaction.emoji)
-						log.info("reaction - '{}'".format(emoji_str))
-						if(emoji_str != "\u274c"):
-							continue
-						all_users = await reaction.users().flatten()
-						for user in all_users:
-							if(user.id is not ctx.author.id):
-								continue
-							tgs_job_cancel(await self.get_address(ctx.guild), await self.get_token(ctx), instance, job.Id)
-							log.info("Requested cancellation")
-							we_canceled = True
+    @tgslink.group()
+    async def dm(self, ctx):
+        pass
 
-				job = tgs_job_get(await self.get_address(ctx.guild), await self.get_token(ctx), instance, job.Id)
-				await msg.edit(content="```\nProgress: {}%\nStage: {}\n```\n".format(job.Progress, job.Stage or "N/A"))
-			await msg.edit(content="Deployment {}".format(["Failed", "Completed"][job.ok()]))
-			if(not job.ok()):
-				await msg.reply("Additional details: ```\n{}\n```".format(job.ExceptionDetails))
+    @dm.command()
+    async def deploy(self, ctx: commands.Context, instance=1):
+        try:
+            job = tgs_dm_deploy(await self.get_address(ctx.guild), await self.get_token(ctx), instance)
+            msg: Message = await ctx.reply("```Caching```\n")
 
-		except TgsModel_ErrorMessageResponse as err:
-			await ctx.reply("Failed to request deployment: {}|{}".format(err._status_code, err.Message))
+            while not job.StoppedAt:
+                await asyncio.sleep(0.5)
+                job = tgs_job_get(await self.get_address(ctx.guild), await self.get_token(ctx), instance, job.Id)
+                await msg.edit(content="```\nProgress: {}%\nStage: {}\n```\n".format(job.Progress, job.Stage or "N/A"))
 
-	@tgslink.group()
-	async def repo(self, ctx): pass
+            if job.ok():
+                await msg.edit(content="Deployment Completed")
+            elif job.Cancelled:
+                await msg.edit(content="Deployment Cancelled")
+            else:
+                await msg.edit(content="Deployment Failed\n```{}```\n".format(job.ExceptionDetails))
 
-	@repo.command()
-	async def active_tms(self, ctx, instance = 1):
-		try:
-			resp = tgs_repo_status(await self.get_address(ctx.guild), await self.get_token(ctx), instance)
-			reply = "Active TMs:\n```\n"
-			for tm in resp.RevisionInformation.ActiveTestMerges:
-				reply += "#{} - {} - @{}\n".format(tm.Number, tm.TitleAtMerge, tm.TargetCommitSha)
-			reply += "```\n"
-			await ctx.reply(reply)
-		except TgsModel_ErrorMessageResponse as err:
-			await ctx.reply("Failed to query TMs: {}|{}".format(err._status_code, err.Message))
+        except TgsModel_ErrorMessageResponse as err:
+            await ctx.reply("Failed to request deployment: {}|{}".format(err._status_code, err.Message))
 
-	@repo.command()
-	async def update_active_tms(self, ctx, instance = 1):
-		try:
-			if(tgs_repo_update_tms(await self.get_address(ctx.guild), await self.get_token(ctx), instance)):
-				await ctx.reply("Updated all TMs")
-			else:
-				await ctx.reply("Failed to update TMs")
-		except TgsModel_ErrorMessageResponse as err:
-			await ctx.reply("Failed to update TMs: {}|{}".format(err._status_code, err.Message))
+    @tgslink.group()
+    async def repo(self, ctx):
+        pass
+
+    @repo.command()
+    async def active_tms(self, ctx, instance=1):
+        try:
+            resp = tgs_repo_status(await self.get_address(ctx.guild), await self.get_token(ctx), instance)
+            reply = "Active TMs:\n```\n"
+            for tm in resp.RevisionInformation.ActiveTestMerges:
+                reply += "#{} - {} - @{}\n".format(tm.Number, tm.TitleAtMerge, tm.TargetCommitSha)
+            reply += "```\n"
+            await ctx.reply(reply)
+        except TgsModel_ErrorMessageResponse as err:
+            await ctx.reply("Failed to query TMs: {}|{}".format(err._status_code, err.Message))
+
+    @repo.command()
+    async def update_active_tms(self, ctx, instance=1):
+        try:
+            if tgs_repo_update_tms(await self.get_address(ctx.guild), await self.get_token(ctx), instance):
+                await ctx.reply("Updated all TMs")
+            else:
+                await ctx.reply("Failed to update TMs")
+        except TgsModel_ErrorMessageResponse as err:
+            await ctx.reply("Failed to update TMs: {}|{}".format(err._status_code, err.Message))
