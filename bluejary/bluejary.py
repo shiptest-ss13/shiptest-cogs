9052719291dd1a99a8e16e36d4aca0b0f4d414b3
ZephyrTFA <matthew@tfaluc.com>
2022-09-01 08:52:54 -0400

    this might be the issue

--- bluejary/bluejary.py
+++ bluejary/bluejary.py
@@ -3,6 +3,9 @@ from discord import Message
 
 
 class BluejaryBot(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
     @commands.Cog.listener()
     async def on_message(self, message: Message):
         if "bluejary" not in message.content:
