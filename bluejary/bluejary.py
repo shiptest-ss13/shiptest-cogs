5a9e362a4fa54a41e9c5ac320e05efb9cb95ef55
ZephyrTFA <matthew@tfaluc.com>
2022-09-27 17:22:08 -0400

    shit

--- bluejary/bluejary.py
+++ bluejary/bluejary.py
@@ -1,190 +1,191 @@
+import asyncio
 from datetime import datetime
 import logging
+from typing import Union
 from redbot.core import commands, Config, checks
-from discord import Message, RawReactionActionEvent, TextChannel, AllowedMentions, Embed, Reaction
+from discord import Message, RawReactionActionEvent, RawReactionClearEvent, RawReactionClearEmojiEvent, Embed
+from .message_info import MessageInfo
 
 log = logging.getLogger("red.bluejary")
 
 
 class BluejaryBot(commands.Cog):
+    im_doing_shit = False
+    old_updates = {}
+
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-        self.config = Config.get_conf(self, 3257141233194, force_registration=True)
+        self.config = Config.get_conf(self, 3257141233294, force_registration=True)
 
         def_cfg = {
-            "emoji_id": None,
-            "board_id": None,
-            "board_req": None,
+            "id_emoji": None,
+            "id_board": None,
+            "board_count": None,
             "board_map": None,
-            "message_map": None,
-            "ignored": None,
         }
 
         self.config.register_guild(**def_cfg)
 
     @commands.group()
-    @commands.guild_only()
     @checks.admin()
-    async def bluejary(self, ctx: commands.Context):
+    async def bluejary(self, context):
         pass
 
     @bluejary.command()
-    async def ignore_channel(self, ctx: commands.Context, channel_id=None):
-        await self.assert_defaults(ctx.guild)
-        ignored: list = await self.config.guild(ctx.guild).ignored()
-        if channel_id is None:
-            resp = "Ignored Channels:\n```\n"
-            for channel in ignored:
-                channel_ins: TextChannel = await self.bot.fetch_channel(channel)
-                resp += f"{channel} - {channel_ins.name}\n"
-            resp += "```\n"
-            await ctx.send(resp)
-        elif channel_id in ignored:
-            ignored.remove(channel_id)
-            await ctx.send("No longer ignoring that channel")
+    async def id_emoji(self, ctx: commands.Context, value):
+        cfg = self.config.guild(ctx.guild)
+        try:
+            await cfg.id_emoji.set(int(value))
+            await ctx.send("Updated value")
+        except Exception:
+            await ctx.send("Failed to update value, check your syntax")
+
+    @bluejary.command()
+    async def id_board(self, ctx: commands.Context, value):
+        cfg = self.config.guild(ctx.guild)
+        try:
+            await cfg.id_board.set(int(value))
+            await ctx.send("Updated value")
+        except Exception:
+            await ctx.send("Failed to update value, check your syntax")
+
+    @bluejary.command()
+    async def board_count(self, ctx: commands.Context, value):
+        cfg = self.config.guild(ctx.guild)
+        try:
+            await cfg.board_count.set(int(value))
+            await ctx.send("Updated value")
+        except Exception:
+            await ctx.send("Failed to update value, check your syntax")
+
+    async def set_board_message(self, message: Message, board_message: Message):
+        cfg = self.config.guild(message)
+        map = await cfg.board_map()
+        if not map:
+            map = {}
+        inf: MessageInfo = map.get(message.id, None)
+        if not inf:
+            inf = MessageInfo().set_message(message).set_board_message(board_message)
         else:
-            ignored.append(channel_id)
-            await ctx.send("Now ignoring that channel")
-        await self.config.guild(ctx.guild).ignored.set(ignored)
+            inf.set_board_message(board_message)
+        map[message.id] = inf.to_json()
+        await cfg.board_map.set(map)
 
-    @bluejary.command()
-    async def set_board(self, ctx: commands.Context, channel_id):
-        await self.assert_defaults(ctx.guild)
-        current = await self.config.guild(ctx.guild).board_id()
-        if current == channel_id:
-            await ctx.send("That is already the board!")
-            return
-        board = await self.bot.get_channel(channel_id)
-        if board is None:
-            await ctx.send("Couldnt find that channel, is it the ID of the channel?")
-            return
-        await self.config.guild(ctx.guild).board_id.set(channel_id)
-        await ctx.send("Updated the board!")
+    async def get_board_message(self, message: Message) -> Union[Message, None]:
+        cfg = self.config.guild(message)
+        map = await cfg.board_map()
+        if not map:
+            map = {}
+        inf: MessageInfo = map.get(message.id, None)
+        if not inf:
+            return None
+        return await inf.get_board_message(self)
 
-    @bluejary.command()
-    async def set_emoji(self, ctx: commands.Context, emoji_id):
-        await self.assert_defaults(ctx.guild)
-        current = await self.config.guild(ctx.guild).emoji_id()
-        if current == emoji_id:
-            await ctx.send("That is already the emoji!")
-            return
-        board = await ctx.guild.fetch_emoji(emoji_id)
-        if board is None:
-            await ctx.send("Couldnt find that emoji, is it the ID?")
-            return
-        await self.config.guild(ctx.guild).emoji_id.set(emoji_id)
-        await ctx.send("Updated the board!")
+    async def count_emoji(self, message: Message):
+        emoji = await self.config.guild(message.guild).id_emoji()
+        if not emoji:
+            return 0
 
-    @commands.Cog.listener()
-    async def on_message(self, message: Message):
-        if not message.content:
-            return
-        if not isinstance(message.content, str):
-            return
-        if "bluejary" not in message.content.lower():
-            return
-        if message.author.bot:
-            return
-        if not message.guild:
-            return
-        await message.add_reaction(await message.guild.fetch_emoji(await self.config.guild(message.guild).emoji_id()))
+        board = await self.get_board_message(message)
 
-    @commands.Cog.listener()
+        checking = list()
+        checking.extend(message.reactions)
+        if board:
+            checking.extend(board.reactions)
+        counts = []
+
+        for reaction in checking:
+            if not reaction.custom_emoji:
+                continue
+            if reaction.emoji.id != emoji:
+                continue
+            for user in await reaction.users().flatten():
+                counts.append(user.id)
+        counts = set(counts)
+        return len(counts)
+
+    async def update_message(self, message: Message = None, message_id=None, message_channel=None):
+        if message.id in self.old_updates:
+            last_update: datetime = self.old_updates[message.id]
+            utcnow = datetime.utcnow()
+            delta = last_update - utcnow
+            if delta.total_seconds < 2:
+                return
+            self.old_updates[message.id] = utcnow
+
+        while self.im_doing_shit is False:
+            await asyncio.sleep(0.25)
+        self.im_doing_shit = True
+
+        if message_id:
+            message = await (await self.bot.fetch_channel(message_channel)).fetch_message(message_id)
+
+        cfg = self.config.guild(message.guild)
+        board_channel = await cfg.id_board()
+        count_target = await cfg.board_count()
+
+        if not count_target or not board_channel:
+            self.im_doing_shit = False
+            return
+
+        if message.channel.id == board_channel:
+            self.im_doing_shit = False
+            return
+
+        emoji_count = await self.count_emoji(message)
+        boarded = (emoji_count >= count_target)
+        board_msg = await self.get_board_message(message)
+
+        if not boarded and board_msg:
+            await board_msg.delete()
+            self.im_doing_shit = False
+            return
+
+        if not board_channel:
+            self.im_doing_shit = False
+            return
+
+        if not board_msg:
+            board_msg = await (await self.bot.fetch_channel(board_channel)).send("caching context")
+            await self.set_board_message(message, board_msg)
+            await asyncio.sleep(0.5)
+
+        try:
+            await self.update_board_message(message, board_msg, emoji_count)
+        except Exception:
+            pass
+        self.im_doing_shit = False
+
+    async def update_board_message(self, message: Message, board_message: Message, emojis):
+        emoji = await message.guild.fetch_emoji(await self.config.guild(message.guild).id_emoji())
+        emoji_str = f"{emojis} - <:{emoji.name}:{emoji.id}>\n\n"
+        embie = Embed(type="rich", description=(emoji_str + message.clean_content()), timestamp=message.created_at)
+        embie.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
+        embie.set_footer(f"<#{message.channel.id}> | <a href='{message.jump_url}'>Jump To</a>")
+        if len(message.attachments):
+            embie.url = message.attachments[0].url
+        await board_message.edit(content=None, embed=embie)
+
+    @commands.Cog.listener("on_raw_reaction_add")
     async def on_raw_reaction_add(self, payload: RawReactionActionEvent):
-        await self.update(payload)
+        await self.update_message(message_id=payload.message_id, message_channel=payload.channel_id)
 
-    @commands.Cog.listener()
+    @commands.Cog.listener("on_raw_reaction_remove")
     async def on_raw_reaction_remove(self, payload: RawReactionActionEvent):
-        await self.update(payload)
+        await self.update_message(message_id=payload.message_id, message_channel=payload.channel_id)
 
-    @commands.Cog.listener()
-    async def on_raw_reaction_clear(self, payload: RawReactionActionEvent):
-        await self.update(payload)
+    @commands.Cog.listener("on_raw_reaction_clear")
+    async def on_raw_reaction_clear(self, payload: RawReactionClearEvent):
+        await self.update_message(message_id=payload.message_id, message_channel=payload.channel_id)
 
-    async def assert_defaults(self, guild):
-        cfg = self.config.guild(guild)
-        if await cfg.emoji_id() is None:
-            await cfg.emoji_id.set(979343052411920384)
-        if await cfg.board_req() is None:
-            await cfg.board_req.set(5)
-        if await cfg.board_map() is None:
-            await cfg.board_map.set({})
-        if await cfg.board_id() is None:
-            await cfg.board_id.set(1014906614815404144)
-        if await cfg.message_map() is None:
-            await cfg.message_map.set({})
-        if await cfg.ignored() is None:
-            await cfg.ignored.set([])
+    @commands.Cog.listener("on_raw_reaction_clear_emoji")
+    async def on_raw_reaction_clear_emoji(self, payload: RawReactionClearEmojiEvent):
+        await self.update_message(message_id=payload.message_id, message_channel=payload.channel_id)
 
-    async def update(self, event: RawReactionActionEvent):
-        if not event.guild_id:
+    @commands.Cog.listener("on_message")
+    async def on_message(self, message: Message):
+        if not message.guild or message.author.bot:
             return
-        guild = await self.bot.fetch_guild(event.guild_id)
-        await self.assert_defaults(guild)
-        config = self.config.guild(guild)
-
-        ignored: list = await config.ignored()
-        if event.channel_id in ignored:
-            return
-
-        board_id = await config.board_id()
-        if not board_id:
-            return
-        board: TextChannel = await self.bot.fetch_channel(board_id)
-        if board is None:
-            return
-
-        message_map: dict = await config.message_map()
-        board_map: dict = await config.board_map()
-        message: Message = None
-        board_message: Message = None
-        if event.channel_id == board_id:
-            log.info("board reaction")
-            m_id = board_map[str(event.message_id)]
-            c_id = message_map[str(m_id)]["channel"]
-            message = await (await self.bot.fetch_channel(c_id)).fetch_message(m_id)
-            log.info(f"message id {message.id}")
-            board_message = await board.fetch_message(event.message_id)
-            log.info(f"board id {board_message.id}")
-        else:
-            message = await (await self.bot.fetch_channel(event.channel_id)).fetch_message(event.message_id)
-            try:
-                board_message = await board.fetch_message(message_map[str(message.id)]["board"])
-            except KeyError:
-                pass
-
-        tallying = list()
-        if message.reactions is not None:
-            tallying = tallying + message.reactions
-            log.info(f"added message reactions: {len(message.reactions)}")
-        if board_message is not None and board_message.reactions is not None:
-            log.info(f"added board reactions: {len(board_message.reactions)}")
-            tallying = tallying + board_message.reactions
-
-        counted = list()
-        emoji_id = await config.emoji_id()
-        emoji = await guild.fetch_emoji(emoji_id)
-        for react in tallying:
-            react: Reaction
-            if not react.custom_emoji or isinstance(react.emoji, str):
-                continue
-            if react.emoji.id != emoji_id:
-                continue
-            for reactee in await react.users().flatten():
-                counted.append(reactee.id)
-        total = len(set(counted))
-        should_board = (total >= await config.board_req())
-
-        if should_board:
-            if not board_message:
-                board_message = await board.send("caching")
-                board_map[str(board_message.id)] = message.id
-                message_map[str(message.id)] = {"board": board_message.id, "channel": message.channel.id}
-            embed: Embed = Embed(type="rich", timestamp=datetime.utcnow(), description=message.content, title=message.author.display_name).set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
-            await board_message.edit(content=f"{total} <:{emoji.name}:{emoji.id}>s", allowed_mentions=AllowedMentions.none(), embed=embed)
-        elif board_message is not None:
-            board_map.pop(str(board_message.id))
-            message_map.pop(str(message.id))
-            await board_message.delete()
-        await config.board_map.set(board_map)
-        await config.message_map.set(message_map)
+        emoji = await message.guild.fetch_emoji(await self.config.guild(message.guild).id_emoji())
+        if emoji.name in message.content:
+            await message.add_reaction(emoji)
---
+++ bluejary/message_info.py
@@ -0,0 +1,68 @@
+from inspect import ismethod
+from json import JSONDecoder, JSONEncoder
+from discord import Message, TextChannel, NotFound
+from .bluejary import BluejaryBot
+from typing import Union
+
+
+class MessageInfo:
+    message_id: int
+    message_channel: int
+    message_message: Message
+
+    board_id: int
+    board_channel: int
+    board_message: Message
+
+    def set_message(self, message: Message):
+        self.message_id = message.id
+        self.message_channel = message.channel.id
+        self.message_message = message
+        return self
+
+    def set_board_message(self, board_message: Message):
+        if not board_message or not isinstance(board_message, Message):
+            self.board_id = None
+            self.board_channel = None
+            self.board_message = None
+            return self
+
+        self.board_id = board_message.id
+        self.board_channel = board_message.channel.id
+        self.board_message = board_message
+        return self
+
+    async def get_message(self, bot: BluejaryBot, fetch=False) -> Union[Message, None]:
+        try:
+            channel: TextChannel = await bot.fetch_channel(self.message_channel)
+            self.message_message = await channel.fetch_message(self.message_id)
+        except NotFound:
+            return None
+        return self.message_message
+
+    async def get_board_message(self, bot: BluejaryBot, fetch=False) -> Union[Message, None]:
+        try:
+            channel: TextChannel = await bot.fetch_channel(self.board_channel)
+            self.board_message = await channel.fetch_message(self.board_id)
+        except NotFound:
+            return None
+        return self.board_message
+
+    def to_json(self) -> str:
+        ret = {}
+        for k in dir(self):
+            if k.startswith("_"):
+                continue
+            v = getattr(self, k)
+            if ismethod(v):
+                continue
+            ret[k] = v
+        return JSONEncoder().encode(ret)
+
+    @classmethod
+    def from_json(cls, json):
+        inf = MessageInfo()
+        ret: dict = JSONDecoder().decode(json)
+        for k, v in ret:
+            setattr(inf, k, v)
+        return inf
