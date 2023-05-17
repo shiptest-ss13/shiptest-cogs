09a6cc5d2346fbb3c8418d595ada99e908154ea7
Mark Suckerberg <mark@suckerberg.gay>
2023-05-17 14:45:25 -0500

    fucked up

--- ss13commands/ss13commands.py
+++ ss13commands/ss13commands.py
@@ -197,7 +197,7 @@ class SS13Commands(commands.Cog):
         else:
             embed = discord.Embed(title="__Crew Manifest:__", description="No crewmembers found! Is the server online?", color=await ctx.embed_color())
 
-        await message.edit(embed=embed)
+        await message.edit(content=null, embed=embed)
 
     @commands.guild_only()
     @commands.hybrid_command()
