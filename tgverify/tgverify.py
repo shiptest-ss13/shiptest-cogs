d87ca28f36a1500d146c836cdeda88de158f0fa9
Mark Suckerberg <mark@suckerberg.gay>
2023-07-22 12:31:34 -0500

    one final tweak

--- tgverify/tgverify.py
+++ tgverify/tgverify.py
@@ -464,7 +464,7 @@ class TGverify(BaseCog):
         fuck = f"Congrats {ctx.author} your verification is complete, but you do not have {min_required_living_minutes} minutes in game as a living crew member (you have {player['living_time']}), so you may not have access to all channels. You can always verify again later by simply doing `?verify` and if you have enough minutes, you will gain access to the remaining channels"
         if successful:
             fuck = f"Congrats {ctx.author} your verification is complete"
-        return await message.edit(content=fuck, color=0xFF0000)
+        return await message.edit(content=fuck)
 
     @app_commands.command(name="verify")
     @app_commands.guild_only()
