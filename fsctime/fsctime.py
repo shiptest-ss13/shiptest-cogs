422c53a9de03b909211528dfb08bf001c4df696d
Mark Suckerberg <mark@suckerberg.gay>
2024-01-04 09:56:07 -0600

    oops x2

--- fsctime/fsctime.py
+++ fsctime/fsctime.py
@@ -50,64 +50,64 @@ class FSCTime(commands.Cog):
     def cog_unload(self):
         self.serv.cancel()
 
-@commands.hybrid_command()
-async def fsctime(self, ctx):
-    """
-    Displays the current time in FSC
-    """
-    await ctx.send(await get_date())
+    @commands.hybrid_command()
+    async def fsctime(self, ctx):
+        """
+        Displays the current time in FSC
+        """
+        await ctx.send(await get_date())
 
-@commands.hybrid_command()
-@checks.admin_or_permissions(manage_guild=True)
-async def setchannel(self, ctx, channel: discord.TextChannel):
-    """
-    Sets the channel to post the time in
-    """
-    cfg = self.config.guild(ctx.guild)
-    await cfg.channel_id.set(channel.id)
-    await ctx.send("Channel set!")
+    @commands.hybrid_command()
+    @checks.admin_or_permissions(manage_guild=True)
+    async def setchannel(self, ctx, channel: discord.TextChannel):
+        """
+        Sets the channel to post the time in
+        """
+        cfg = self.config.guild(ctx.guild)
+        await cfg.channel_id.set(channel.id)
+        await ctx.send("Channel set!")
 
-@commands.hybrid_command()
-@checks.admin_or_permissions(manage_guild=True)
-async def setmessage(self, ctx, message: discord.Message):
-    """
-    Sets the message to update
-    """
-    cfg = self.config.guild(ctx.guild)
-    await cfg.message_id.set(message.id)
-    await ctx.send("Message set!")
+    @commands.hybrid_command()
+    @checks.admin_or_permissions(manage_guild=True)
+    async def setmessage(self, ctx, message: discord.Message):
+        """
+        Sets the message to update
+        """
+        cfg = self.config.guild(ctx.guild)
+        await cfg.message_id.set(message.id)
+        await ctx.send("Message set!")
 
-async def time_update_loop(self):
-    for guild in self.bot.guilds:
-        cfg = self.config.guild(guild)
+    async def time_update_loop(self):
+        for guild in self.bot.guilds:
+            cfg = self.config.guild(guild)
 
-        message = await cfg.message_id()
-        channel = await cfg.channel_id()
-        cached: discord.Message
+            message = await cfg.message_id()
+            channel = await cfg.channel_id()
+            cached: discord.Message
 
-        if(message == None):
-            if(isinstance(message, str)): 
-                message = int(message)
-            cached = await channel.send("caching initial context")
-            await cfg.message_id.set(cached.id)
-        else:
-            try:
-                cached = await channel.fetch_message(message)
-            except(discord.NotFound):
+            if(message == None):
+                if(isinstance(message, str)): 
+                    message = int(message)
                 cached = await channel.send("caching initial context")
                 await cfg.message_id.set(cached.id)
+            else:
+                try:
+                    cached = await channel.fetch_message(message)
+                except(discord.NotFound):
+                    cached = await channel.send("caching initial context")
+                    await cfg.message_id.set(cached.id)
 
-        await cached.edit(content=await get_date())
+            await cached.edit(content=await get_date())
 
-async def get_date():
-    timestamp = datetime.utcnow().timestamp()
-    days = floor(timestamp / UNIX_DAYS)
-    years = floor(days / 365) + 481
+    async def get_date():
+        timestamp = datetime.utcnow().timestamp()
+        days = floor(timestamp / UNIX_DAYS)
+        years = floor(days / 365) + 481
 
-    day_of_year = days % 365 + 1
-    month_of_year = floor(day_of_year / 28)
+        day_of_year = days % 365 + 1
+        month_of_year = floor(day_of_year / 28)
 
-    day_of_month = day_of_year % 28 + 1
-    month_name = MONTH_NAMES[month_of_year]
+        day_of_month = day_of_year % 28 + 1
+        month_name = MONTH_NAMES[month_of_year]
 
-    return f"{month_name} {day_of_month}, {years} FSC"
+        return f"{month_name} {day_of_month}, {years} FSC"
